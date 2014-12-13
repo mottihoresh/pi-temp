0798b5828dfd960b5d7ed87cbf1c5ed46cae5d75
Motti Horesh <mottihoresh@gmail.com>
2014-12-12 20:33:29 -0500

    temp monitor main page

--- client/app/tempMonitor/tempMonitor.controller.js
+++ client/app/tempMonitor/tempMonitor.controller.js
@@ -1,6 +1,17 @@
 'use strict';
 
 angular.module('tempMonitorApp')
-  .controller('TempMonitorCtrl', function ($scope) {
-    $scope.message = 'Hello';
-  });
+    .controller('TempMonitorCtrl', function ($scope, $interval) {
+        $scope.message = 'Hello';
+
+        $scope.sensors = [];
+
+
+        $interval(function(){
+            _.each($scope.sensors, function(sensor, index){
+                $scope.sensors[index].reading = Math.floor(Math.random()*100);
+            });
+
+        },1000);
+
+    });
