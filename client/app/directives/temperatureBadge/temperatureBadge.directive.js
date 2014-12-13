5348365a01e1523deee847499506ff099b2eb8fc
Motti Horesh <mottihoresh@gmail.com>
2014-12-12 20:32:05 -0500

    Temperature badge basic template

--- client/app/directives/temperatureBadge/temperatureBadge.directive.js
+++ client/app/directives/temperatureBadge/temperatureBadge.directive.js
@@ -1,11 +1,18 @@
 'use strict';
 
 angular.module('tempMonitorApp')
-  .directive('temperatureBadge', function () {
-    return {
-      templateUrl: 'app/directives/temperatureBadge/temperatureBadge.html',
-      restrict: 'EA',
-      link: function (scope, element, attrs) {
-      }
-    };
-  });
+    .directive('temperatureBadge', function () {
+        return {
+            templateUrl: 'app/directives/temperatureBadge/temperatureBadge.html',
+            restrict: 'EA',
+            scope: { sensor: '=temperatureBadge' },
+            link: function (scope, element, attrs) {
+
+                scope.$watch('sensor', function(walks) {
+                    //console.log(scope.sensor, walks);
+                });
+
+            }
+        };
+    });
+
