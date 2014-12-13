1c18616fc928980f22dce91a1105c04a610613c4
Motti Horesh <mottihoresh@gmail.com>
2014-12-12 20:27:23 -0500

    initial commit

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('tempMonitorApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
